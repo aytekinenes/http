b0e0e96ed3c0827b49458eacbd621ac5685cf548
aytekinenes <enesaytekin@gmail.com>
2024-05-01 23:30:51 +0300

    Added full json

--- main.cpp
+++ main.cpp
@@ -1,38 +1,57 @@
 #include <iostream>
 #include <curl/curl.h>
-#include "json.hpp"
+#include <json.hpp>
 
 using json = nlohmann::json;
-using namespace std;
+
+
+size_t waitingForData(void *ptr, size_t size, size_t nmemb, std::string *result)
+{
+    size_t result_length = size * nmemb;
+    result->append((char *)ptr, result_length); 
+    return result_length;
+}
 
 int main()
 {
-    cout << "curl started"<< endl;
+    std::cout << "curl started" << std::endl;
     CURL *curl;
-    CURLcode result;
+    CURLcode res;
+    std::string result;
 
     curl = curl_easy_init();
-    if(curl == NULL)
+    if (curl == NULL)
     {
-        cout<< "curl not found"<< endl;
+        std::cout << "curl not found" << std::endl;
         return -1;
     }
-    curl_easy_setopt(curl, CURLOPT_URL, "https://jsonplaceholder.typicode.com/posts");
-
-    result = curl_easy_perform(curl);
-
-        // JSON verilerini parse edelim
-    if (result != CURLE_OK) 
+    if (curl)
     {
-        cout << "curl error: " << curl_easy_strerror(result) << endl;
-    } 
-    else 
-    {
-        json responseData = json::parse(curl_easy_strerror(result));
-        for (const auto& item : responseData) 
+        curl_easy_setopt(curl, CURLOPT_URL, "https://jsonplaceholder.typicode.com/posts");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, waitingForData);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK)
         {
-            cout << "User ID: " << item["userId"] << ", ID: " << item["id"] << ", Title: " << item["title"] << endl;
+            std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
         }
+        else
+        {
+            std::cout << "Getting data:\n" << result << std::endl;
+
+            json data = json::parse(result);
+
+            for (const auto &post : data)
+            {
+                std::cout << "User ID: " << post["userId"]<< std::endl;
+                std::cout << "Id: " << post["id"]<< std::endl;
+                std::cout << "Title: " << post["title"] << std::endl;
+                std::cout << "Body: "<< post["body"]<< std::endl;
+            }
+        }
+
+        curl_easy_cleanup(curl);
     }
 
     return 0;
